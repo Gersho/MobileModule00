03edec60d0dd0f67e4123788b3e9a2d4290d64bb
Gersho <zennoune.karim@outlook.fr>
2024-06-04 21:41:41 +0200

    calc skin OK

--- ex02/lib/main.dart
+++ ex02/lib/main.dart
@@ -3,158 +3,149 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ super.key });
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: CalcApp(),
-
     );
   }
 }
 
-class CalcApp extends StatelessWidget
-{
+
+
+class CalcApp extends StatelessWidget {
   const CalcApp({super.key});
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-
-      appBar: AppBar(
-        title: const Text("Calculator"),
-        backgroundColor: Color.fromARGB(255, 3, 126, 156),
-        centerTitle: true,
-      ),
-      body: const Column (
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          CalcDisplay(),
-          CalcPad()
-        ],
-      )
-    );
+    
+    
+    Size size = MediaQuery.of(context).size;
+    
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculator",maxLines: 1,style:  TextStyle(
+                fontSize: size.height * 0.1,
+              )),
+          backgroundColor: Color.fromARGB(255, 3, 126, 156),
+          centerTitle: true,
+          toolbarHeight: size.height * 0.1,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [CalcDisplay(),
+                     
+                     Container(
+                       
+                       
+                             color: Colors.blue,
+      height: size.height * 0.60,
+      width: size.width,
+                       
+                     child: CalcPad())
+                    ],
+        ));
+  }
 }
+
+
+class CalcButton extends StatelessWidget {
+  final String btn;
+  
+  const CalcButton({
+    required this.btn,
+  });
+
+
+  void displayPressed(String val) {
+    debugPrint(val);
+  }
+
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    
+      Size size = MediaQuery.of(context).size;
+    
+
+    
+  return  SizedBox(
+  width: size.width / 5,
+  height: (size.height * 0.65) / 6,
+  child: ElevatedButton(  
+                  onPressed: () { displayPressed(btn); },
+                  child:  Text(btn, style: TextStyle( fontSize: size.height * 0.06, )),
+                  )
+);
+
+  }
 }
 
-class CalcPad extends StatelessWidget
-{
+class CalcPad extends StatelessWidget {
   const CalcPad({super.key});
 
-  void displayPressed(String val)
-  {
+  void displayPressed(String val) {
     debugPrint(val);
   }
 
   @override
   Widget build(BuildContext context) {
-
-  return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            
-
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(  onPressed: () { displayPressed("C"); },
-                  child: const Text('C', style: TextStyle( fontSize: 30, )),),
-
-                  ElevatedButton(  onPressed: () { displayPressed("AC"); },
-                  child: const Text('AC', style: TextStyle( fontSize: 20, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed(""); },
-                  child: const Text('0', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed(""); },
-                  child: const Text('0', style: TextStyle( fontSize: 30, )),)
-
-                ],
-              ),
-
-
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(  onPressed: () { displayPressed("7"); },
-                  child: const Text('7', style: TextStyle( fontSize: 30, )),),
-
-                  ElevatedButton(  onPressed: () { displayPressed("8"); },
-                  child: const Text('8', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed("9"); },
-                  child: const Text('9', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed("/"); },
-                  child: const Text('/', style: TextStyle( fontSize: 30, )),)
-                ],
-              ),
-
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(  onPressed: () { displayPressed("4"); },
-                  child: const Text('4', style: TextStyle( fontSize: 30, )),),
-
-                  ElevatedButton(  onPressed: () { displayPressed("5"); },
-                  child: const Text('5', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed("6"); },
-                  child: const Text('6', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed("*"); },
-                  child: const Text('*', style: TextStyle( fontSize: 30, )),)
-                ],
-              ),
-
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(  onPressed: () { displayPressed("1"); },
-                  child: const Text('1', style: TextStyle( fontSize: 30, )),),
-
-                  ElevatedButton(  onPressed: () { displayPressed("2"); },
-                  child: const Text('2', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed("3"); },
-                  child: const Text('3', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed("-"); },
-                  child: const Text('-', style: TextStyle( fontSize: 30, )),)
-                ],
-              ),
-
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(  onPressed: () { displayPressed("1"); },
-                  child: const Text('0', style: TextStyle( fontSize: 30, )),),
-
-                  ElevatedButton(  onPressed: () { displayPressed("2"); },
-                  child: const Text('.', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed("3"); },
-                  child: const Text('=', style: TextStyle( fontSize: 30, )),),
-
-                   ElevatedButton(  onPressed: () { displayPressed("+"); },
-                  child: const Text('+', style: TextStyle( fontSize: 30, )),)
-                ],
-              ),
+            CalcButton(btn: "C"),
+            CalcButton(btn: "AC"),
+            CalcButton(btn: "@"),
+            CalcButton(btn: "/"),
           ],
-        );
-
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CalcButton(btn: "7"),
+            CalcButton(btn: "8"),
+            CalcButton(btn: "9"),
+            CalcButton(btn: "*"),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CalcButton(btn: "4"),
+            CalcButton(btn: "5"),
+            CalcButton(btn: "6"),
+            CalcButton(btn: "-"),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CalcButton(btn: "1"),
+            CalcButton(btn: "2"),
+            CalcButton(btn: "3"),
+            CalcButton(btn: "+"),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CalcButton(btn: "0"),
+            CalcButton(btn: "00"),
+            CalcButton(btn: "."),
+            CalcButton(btn: "="),
+          ],
+        ),
+      ],
+    );
+  }
 }
-}
-
-
-
 
 class CalcDisplay extends StatefulWidget {
-  const CalcDisplay({ super.key });
+  const CalcDisplay({super.key});
   @override
   State<CalcDisplay> createState() => CalcDisplayState();
 }
@@ -162,54 +153,30 @@ class CalcDisplay extends StatefulWidget {
 class CalcDisplayState extends State<CalcDisplay> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Container(
-        alignment: Alignment.topRight,
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text("0",
+      alignment: Alignment.topRight,
+      color: Colors.yellow,
+      height: size.height * 0.30,
+      width: size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+           Text("0",
+              maxLines: 1,
+              textAlign: TextAlign.right,
+              style:  TextStyle(
+                fontSize: size.height * 0.1,
+              )),
+          Text("0",
               maxLines: 1,
               textAlign: TextAlign.right,
               style: TextStyle(
-                    fontSize: 32,
-                  )),
-             
-            Text("0",
-              maxLines: 1,
-              textAlign: TextAlign.right,
-              style: TextStyle(
-                    fontSize: 32,
-                  )),
-              ],
-          ),
-      );
+                fontSize: size.height * 0.1,
+              )),
+        ],
+      ),
+    );
   }
 }
-
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key});
-//   @override
-//   MyHomePageState createState() => MyHomePageState();
-// }
-
-// class MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-
-//       appBar: AppBar(
-//         title: const Text("Calculator"),
-//         backgroundColor: Color.fromARGB(255, 3, 126, 156),
-//         centerTitle: true,
-//       ),
-
-
-//       body: const CalcDisplay(),
-//     );
-//   }
-
-
-// }
-
 
